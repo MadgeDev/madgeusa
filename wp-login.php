88f4d604ae21a3eed6733472dfd09874d3e9b730
MadgeDev <82634478+MadgeDev@users.noreply.github.com>
2021-05-02 17:24:31 +0800

    Update06
    Contact Form

--- wp-login.php
+++ wp-login.php
@@ -96,7 +96,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="//madgeusa.netlify.app/wp-includes/js/wp-util.min.js?ver=5.7.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"8499faa40f"};
+var userProfileL10n = {"user_id":"0","nonce":"20f153af8b"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
